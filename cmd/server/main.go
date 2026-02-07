cbf8c98b641e7b54c55be51edb7471a4dc67d3ec
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-02-06 23:42:03 -0500

    quicksave

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"github.com/rabbitmq/amqp091-go"
+	"fmt"
+)
 
 func main() {
+	connString := "amqp://guest:guest@localhost:5672/"
+	conn := amqp.Dial(connString)
+	defer conn.Close()
 	fmt.Println("Starting Peril server...")
 }
