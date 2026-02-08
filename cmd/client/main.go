de8e05774b962f6064608fd75bce478a46137aa0
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-02-08 08:59:55 -0500

    quicksave

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/NebojsaJovanovic95/learn-pub-sub-starter/internal/pubsub"
+	"github.com/NebojsaJovanovic95/learn-pub-sub-starter/internal/routing"
+	"github.com/NebojsaJovanovic95/learn-pub-sub-starter/internal/gamelogic"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	rabbitURL := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbitURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	username := gamelogic.ClientWelcome()
+	
+	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
+
+	ch, _, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		queueName,
+		routing.PauseKey,
+		pubsub.Transient,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ch.Close()
+
+	fmt.Println("Client running. Press Ctrl+C to exit.")
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	<-sig
 }
---
+++ internal/pubsub/queue.go
@@ -0,0 +1,58 @@
+package pubsub
+
+import (
+	"log"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/NebojsaJovanovic95/learn-pub-sub-starter/internal/pubsub"
+	"github.com/NebojsaJovanovic95/learn-pub-sub-starter/internal/routing"
+)
+
+type SimpleQueueType int
+
+const (
+	Durable SimpleQueueType = iota
+	Transient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	durable := queueType == Durable
+	autoDelete := queueType == Transient
+	exclusive := queueType == Transient
+
+	q, err := ch.QueueDeclare(
+		queueName,
+		durable,
+		autoDelete,
+		exclusive,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = ch.QueueBind(
+		q.Name,
+		key,
+		exclusive,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return ch, q, nil
+}
