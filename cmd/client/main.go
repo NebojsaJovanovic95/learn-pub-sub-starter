394351c65396ab5972e1f3f9c2a9f652c27f38e0
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-02-08 11:23:33 -0500

    repl finished

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -22,10 +22,6 @@ func main() {
 	defer conn.Close()
 
 	username, err := gamelogic.ClientWelcome()
-	if err != nil {
-		log.Fatal(err)
-	}
-	
 	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
 
 	ch, _, err := pubsub.DeclareAndBind(
@@ -42,7 +38,62 @@ func main() {
 
 	fmt.Println("Client running. Press Ctrl+C to exit.")
 
+	state := gamelogic.NewGameState(username)
+
+	// Handle Ctrl+C
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-	<-sig
+	go func() {
+		<-sig
+		fmt.Println("\nClient exiting...")
+		os.Exit(0)
+	}()
+
+	// REPL loop
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "spawn":
+			id := state.CommandSpawn(words)
+			fmt.Printf("Spawned unit with ID %d\n", id)
+		case "move":
+			mv, err := state.CommandMove(words)
+			if err != nil {
+				fmt.Println("Error moving unit:", err)
+			} else {
+				fmt.Printf("Move successful! Moved %d unit(s) to %s\n", len(mv.Units), mv.ToLocation)
+			}
+		case "status":
+			state.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+		case "pause":
+			fmt.Println("Sending pause message...")
+			err := pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: true,
+			})
+			if err != nil {
+				log.Println("Error publishing pause:", err)
+			}
+		case "resume":
+			fmt.Println("Sending resume message...")
+			err := pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: false,
+			})
+			if err != nil {
+				log.Println("Error publishing resume:", err)
+			}
+		case "quit":
+			fmt.Println("Exiting client...")
+			return
+		default:
+			fmt.Println("Unknown command:", words[0])
+		}
+	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/NebojsaJovanovic95/learn-pub-sub-starter/internal/pubsub"
 	"github.com/NebojsaJovanovic95/learn-pub-sub-starter/internal/routing"
@@ -17,54 +18,49 @@ func main() {
 	}
 	defer conn.Close()
 
-	username := gamelogic.ClientWelcome()
-	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
-
-	ch, _, err := pubsub.DeclareAndBind(
-		conn,
-		routing.ExchangePerilDirect,
-		queueName,
-		routing.PauseKey,
-		pubsub.Transient,
-	)
+	ch, err := conn.Channel()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ch.Close()
 
-	fmt.Println("Client running. Press Ctrl+C to exit.")
+	err = ch.ExchangeDeclare(
+		routing.ExchangePerilTopic,
+		"topic",
+		true,  // durable
+		false, // autoDelete
+		false, // internal
+		false, // noWait
+		nil,
+	)
+	if err != nil {
+		log.Fatal("Failed to declare peril_topic exchange:", err)
+	}
 
-	state := gamelogic.NewGameState(username)
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilTopic,
+		"game_logs",
+		routing.GameLogSlug + ".*",
+		pubsub.Durable,
+	)
+	if err != nil {
+		log.Fatal("Failed to declare/bind game_logs queue:", err)
+	}
 
-	for ;; {
+	fmt.Println("Starting Peril server...")
+	gamelogic.PrintServerHelp()
+
+	// REPL loop
+	for {
 		words := gamelogic.GetInput()
 		if len(words) == 0 {
 			continue
 		}
 
 		switch words[0] {
-		case "spawn":
-			id, err := state.CommandSpawn(words)
-			if err != nil {
-				fmt.Println("Error spawning unit:", err)
-			} else {
-				fmt.Println("Spawned unit with ID %d\n", err)
-			}
-		case "move":
-			err := state.CommandMove(words)
-			if err != nil {
-				fmt.Println("Error moving unit:", err)
-			} else {
-				fmt.Println("Move successfully!")
-			}
-		case "status":
-			state.CommandStatus()
-		case "help":
-			gamelogic.PrintClientHelp()
-		case "spam":
-			fmt.Println("Spamming not allowed yet!")
 		case "pause":
-			fmt.Println("sending pause message...")
+			fmt.Println("Sending pause message...")
 			err := pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
 				IsPaused: true,
 			})
@@ -77,13 +73,13 @@ func main() {
 				IsPaused: false,
 			})
 			if err != nil {
-				log.Println("Error publishing resume...", err)
+				log.Println("Error publishing resume:", err)
 			}
 		case "quit":
 			fmt.Println("Exiting server...")
 			return
 		default:
-			fmt.Println("Unknown commands:", words[0])
+			fmt.Println("Unknown command:", words[0])
 		}
 	}
 }
